312100e1e9839096fb29a0ca7c53d4c80639d433
Daniel Miller <danielemiller@live.com>
2021-01-02 13:34:05 -0500

    Fix unused variables

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import DashboardCard from '../components/DashboardCard';
 import Header from '../components/Header';
-import LineGraph from '../components/LineGraph';
-import NewsFeed from '../components/NewsFeed';
 import Stats from '../components/Stats';
-import TimeLine from '../components/TimeLine';
 
 const Dashboard = () => {
     return (
